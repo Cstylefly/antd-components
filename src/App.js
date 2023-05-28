b9659795202b7798d50373dcdbab0038e25bb0ca
ccc <169713467@qq.com>
2023-05-28 10:22:32 +0800

    'feat:修改'

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-/** @format */
+/**
+ * @format
+ * @auto tangcong
+ * @date 2023/05/28
+ */
 
 import React from 'react';
 import { Button } from 'antd';
 
 function App() {
-  const a = 'jaaad';
-
-  const quotePropsDemo = {
-    a: 1,
-    'b-2': 2,
-    c: 3
-  };
-
-  const name = x => {
-    if (a == quotePropsDemo) {
-      console.log('dsaf');
-    }
-    console.log('dasfsa');
-  };
-
-  const j = 1;
-
-  const handleClick = () => 'a';
-
   return (
     <>
       <Button
