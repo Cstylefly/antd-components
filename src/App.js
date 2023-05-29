f3fa41a6141fb608742501b412bf90e21bee181c
ccc <169713467@qq.com>
2023-05-29 22:32:42 +0800

    feat:修改

--- src/App.js
+++ src/App.js
@@ -4,16 +4,45 @@
  * @date 2023/05/28
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'antd';
 
 function App() {
+  const [domRoot, setDomRoot] = useState(null);
+
+  useEffect(() => {
+    setDomRoot(document.getElementById('root-div'));
+  }, []);
+
+  const fetchData = () => {
+    return new Promise(resolve => {
+      const res = {
+        code: 200,
+        msg: 'success',
+        data: []
+      };
+      for (let i = 0; i < 100000; i++) {
+        res.data.push(`content-${i + 1}`);
+      }
+      setTimeout(() => {
+        resolve(res);
+      }, 1000);
+    });
+  };
+
   return (
     <>
+      <div id={'root-div'}></div>
       <Button
         type={'primary'}
         onClick={() => {
-          console.log('button');
+          fetchData().then(res => {
+            res.data.forEach(item => {
+              const olItem = document.createElement('li');
+              olItem.innerText = item;
+              domRoot.appendChild(olItem);
+            });
+          });
         }}
       >
         测试button
